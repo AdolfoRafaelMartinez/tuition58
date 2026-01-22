b90fca60a58a988d7f47efa6dd202c8f735bd66e
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2026-01-21 23:40:21 -0600

    success in displaying observed maximum

--- src/services/cli_weather.ts
+++ src/services/cli_weather.ts
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 
 interface CliWeatherResult {
-    maxTemp: string | null;
-    maxTempTime: string | null;
+    when_max: string | null;
+    temp_max: string | null;
     error: string | null;
 }
 
@@ -10,30 +10,26 @@ export async function getAustinCliWeather(): Promise<CliWeatherResult> {
     try {
         const response = await fetch('https://forecast.weather.gov/product.php?site=EWX&product=CLI&issuedby=AUS');
         if (!response.ok) {
-            return { maxTemp: null, maxTempTime: null, error: `Failed to fetch weather data. Status: ${response.status}` };
+            return {when_max: "", temp_max: "", error: `Failed to fetch weather data. Status: ${response.status}` };
         }
-
         const text = await response.text();
-
-        const dateRegex = /CLIMATE REPORT\D*\d{1,3}\s\D*\d{1,2}\s\d{4}/;
-        const date_match = text.match(dateRegex);
-        if (!date_match) {
-            return { maxTempTime: null, error: 'Could not get date and time.' };
+        const when_regex = /CLIMATE REPORT\D*\d{1,3}\D*\d*\s\d*/;
+        const when_match = text.match(when_regex)[0].split("\n")[2];
+        if (!when_match) {
+            return {when_max: "", temp_max: "", error: 'Could not get date and time.' };
         }
-        const maxTempTime = date_match[0].split(/\s+/);
-        const maxTempRegex = /MAXIMUM\s*\d{1,2}\s*\d{1,2}:\d{1,2}\s\D{2}/;
-        const temp_match = text.match(maxTempRegex);
-        if (!temp_match) {
-            return { maxTemp: null, error: 'Could not parse max temperature from weather data.' };
+        const temp_max_regex = /MAXIMUM\s*\d{1,2}\s*\d{1,2}:\d{1,2}\s\D{2}/;
+        const temp_max_match= text.match(temp_max_regex)[0].split(/\s+/).join(" ");
+        if (!temp_max_match) {
+            return {when_max: "", temp_max: "", error: 'Could not parse max temperature from weather data.' };
         }
-        const maxTemp = temp_match[0].split(/\s+/);
         return {
-            maxTempTime: split[2] + " " + split[3],
-            maxTemp: split[1],
+            when_max: when_match,
+            temp_max: temp_max_match,
             error: null
         };
     } catch (error) {
         console.error('Error fetching weather data:', error);
-        return { maxTemp: null, maxTempTime: null, error: 'Failed to fetch weather data.' };
+        return { when_max: "", temp_max: "", error: 'Failed to fetch weather data.' };
     }
 }
